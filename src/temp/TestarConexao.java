79b3c8f2f71965b9904c060e0e346fd78cc9d7c2
analiju <jujucarvalho.souza@yahoo.com.br>
2018-07-26 19:09:27 -0300

    Adicionando pacote com classe paraa testar conexao

---
+++ src/temp/TestarConexao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.excecao.ExcecaoPersistencia;
+import util.db.ConnectionManager;
+
+/**
+ *
+ * @author Juliana
+ */
+public class TestarConexao {
+
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, ExcecaoPersistencia {
+        System.out.print("Teste");
+        try {
+            Connection connection = ConnectionManager.getInstance().getConnection();
+
+            String sql = "Select tablename from pg_tables Where schemaname = 'public' ";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+
+            
+            
+
+            ResultSet rs = pstmt.executeQuery();
+            
+            String nome =  "\n";
+
+            while (rs.next()) {
+                nome += rs.getString("tablename")+"\n";
+            }
+
+            rs.close();
+            pstmt.close();
+            connection.close();
+            System.out.println("nome: "+nome);
+
+        } catch (ClassNotFoundException | SQLException e) {
+
+            throw new ExcecaoPersistencia(e.getMessage(), e);
+
+        }
+    }
+}
