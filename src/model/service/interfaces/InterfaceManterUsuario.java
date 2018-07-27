af2e493f9416de28895996c6935b5b39866462f8
eduardo epiphanio <eduardo.epiphanio.moreira@gmail.com>
2018-07-27 11:27:41 -0300

    corigindo

--- src/model/service/interfaces/InterfaceManterUsuario.java
+++ src/model/service/interfaces/InterfaceManterUsuario.java
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model.service.interfaces;
 
-import java.util.List;
-import model.domain.Usuario;
-import util.db.exception.ExcecaoNegocio;
-import util.db.exception.ExcecaoPersistencia;
-
-/**
- *
- * @author Juliana
- */
-public interface InterfaceManterUsuario {
-    public Long cadastrar(Usuario usuario) throws ExcecaoPersistencia, ExcecaoNegocio;
-    public boolean alterar(Usuario usuario) throws ExcecaoPersistencia, ExcecaoNegocio;
-    public boolean excluir(Usuario usuario) throws ExcecaoPersistencia, ExcecaoNegocio;
-    public Usuario pesquisarPorId(Long codUsuario) throws ExcecaoPersistencia;
-    public Usuario pesquisarPorEmail(String txtEmail) throws ExcecaoPersistencia;
-    public List<Usuario> pesquisarTodos() throws ExcecaoPersistencia;
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.service.interfaces;
 
 
@@ -48,4 +18,3 @@ public interface InterfaceManterUsuario {
     public Usuario pesquisarPorId(Long id) throws ExcecaoPersistencia;
     public Usuario getUserLogin(String nome, String senha) throws ExcecaoPersistencia,ExcecaoNegocio;
 }
->>>>>>> 19eae2760b25143367cc2c89742f1866ae5771f3
